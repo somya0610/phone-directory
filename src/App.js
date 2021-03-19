a9f6a65a9566d10bc01dcf73cec5af3ab38c2ca5
Somya <somya0610@gmail.com>
2021-03-18 23:30:22 -0700

    Created ‘Header'

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
+import React from "react";
+import Header from "./Header"
+
 function App() {
+  // let x = 1;
+  // let y = 2;
   return (
     <div>
       {/* <div className="header">
         Phone Directory
-      </div>
+      </div> */}
+      <Header />
       <button> Add </button>
       <div>
         <span> Name </span><br />
         <span> Phone </span>
-      </div> */}
-      <label htmlFor="name">Name: </label>
+      </div>
+      {/* <label htmlFor="name">Name: </label>
       <input id="name" type="text" placeholder="Type Here" defaultValue="Somya" />
+      <input id="name" type="text" placeholder="Type Here" defaultValue={x+y} /> */}
     </div>
+
+    // <div id="module">
+    //   <p>ReactJS</p>
+    // </div>
+
+    // React.createElement("div", {id:"module"},
+    //   React.createElement("p", null, "ReactJS")
+    // )
+
+
   );
 }
 
