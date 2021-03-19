1e9cab79fc1f40086f1cce947a1d8a7d86cac0b9
Somya <somya0610@gmail.com>
2021-03-18 19:26:50 -0700

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 function App() {
   return (
     <div>
-      Phone Directory
+      {/* <div className="header">
+        Phone Directory
+      </div>
+      <button> Add </button>
+      <div>
+        <span> Name </span><br />
+        <span> Phone </span>
+      </div> */}
+      <label htmlFor="name">Name: </label>
+      <input id="name" type="text" placeholder="Type Here" defaultValue="Somya" />
     </div>
   );
 }
